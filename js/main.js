8f1d8ff07eb95c26f31f3aea9c9d1c14de2fc469
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-07-12 10:45:31 +0200

    some progress on paridispari exercise

--- js/main.js
+++ js/main.js
@@ -10,17 +10,105 @@ Dichiariamo chi ha vinto.
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma (una parola che si legge allo stesso modo anche da destra versi sinistra, ad esempio anna, otto)
-
 */
 
-const userNum = parseInt(prompt("Scegli un numero intero da 1 a 5:"));
-const oddOrEven = prompt("Scegli pari o dispari e scrivi la tua scelta:");
+const getUserNum = document.querySelector(".number");
+let userNum = getUserNum.value;
 
-function rng (){
-    randomNum = (Math.floor(Math.random() * 5) + 1);
-    return randomNum;
-}
+const getUserChoice = document.querySelector(".user-choice");
+let userChoice = getUserChoice.value;
 
-let compRandNum = rng();
-console.log(compRandNum);
+const button = document.querySelector(".btn")
+const mainContainer = document.querySelector(".container");
 
+button.addEventListener('click', function (){
+
+    mainContainer.innerHTML = "";
+
+    console.log("User given number: " + userNum);
+    mainContainer.innerHTML += (`<div class="row mb-4">
+                                    <span class="fs-3 fw-bold">Numero scelto dall'utente: <span class"fs-3">${userNum}</span></span>
+                                </div>`);
+    console.log("User choice over odd (dispari) or even (pari): " + userChoice);
+    mainContainer.innerHTML += (`<div class="row mb-4">
+                                    <span class="fs-3 fw-bold">Scelto dispari o pari: <span class"fs-3">${userChoice}</span></span>
+                                </div>`);
+
+    function rng (){
+        randomNum = (Math.floor(Math.random() * 5) + 1);
+        console.log(randomNum);
+        return randomNum;
+    }
+    
+    let computerRandomNumber = rng();
+    console.log("Randomly generated number for computer: " + computerRandomNumber);
+    mainContainer.innerHTML += (`<div class="row mb-4">
+                                    <span class="fs-3 fw-bold">Numero randomicamente generato dal computer: <span class"fs-3">${computerRandomNumber}</span></span>
+                                </div>`);
+
+    const sum = userNum[0] + computerRandomNumber[0];
+    console.log(`Sum of user given number (${userNum}) + computer randomly generated number (${computerRandomNumber}): ${sum}`);
+    mainContainer.innerHTML += (`<div class="row mb-4">
+                                    <span class="fs-3 fw-bold">Somma del numero scelto dall'utente (${userNum}) + il numero generato per il computer (${computerRandomNumber}): <span class"fs-3"> ${sum}</span></span>
+                                </div>`);
+
+    function sumCheck(sum){
+        let sumCheck = "";
+        if(sum % 2 === 0){
+            sumCheck = "pari";
+        }
+        else{
+            sumCheck = "dispari"
+        }
+        return sumCheck;
+    }
+
+    let evenOddSum = sumCheck(sum);
+    console.log("The sum is: " + evenOddSum);
+    mainContainer.innerHTML += (`<div class="row mb-4">
+                                    <span class="fs-3 fw-bold">La somma è: <span class"fs-3"> ${evenOddSum}</span></span>
+                                </div>`);
+
+    function result(userChoice, evenOddSum){
+        let result = "";
+        if(evenOddSum = "pari"){
+            if(userChoice = "Pari"){
+                result = "win";
+                console.log("Hai vinto!")
+            }
+            else{
+                console.log("Hai perso..")
+                result = "lost";
+            }
+        }
+        else{
+            if(userChoice = "Dispari"){
+                result = "win";
+                console.log("Hai vinto!")
+            }
+            else{
+                console.log("Hai perso..")
+                result = "lost";
+            }
+        }
+        return result;
+    }
+    let wonOrLost = result(userChoice, evenOddSum);
+    console.log(wonOrLost);
+
+    if(wonOrLost = "win"){
+        mainContainer.innerHTML += (`<div class="row mb-4">
+                                        <span class="fs-1 fw-bold">HAI VINTO!</span>
+                                    </div>`);
+    }
+    else{
+        mainContainer.innerHTML += (`<div class="row mb-4">
+                                        <span class="fs-1 fw-bold">SFORTUNATAMENTE HAI PERSO..</span>
+                                    </div>`);
+    }
+});
+
+/*
+let wonOrLost = result(oddOrEven, evenOddSum);
+console.log(result);
+*/
